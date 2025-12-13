2224187b60438e1920d04c094be5641035036c71
nghilethanh09052000 <nghilt19411@gmail.com>
2025-12-13 16:30:24 +0700

    Remove testing text

--- staffing_agency_scraper/scraping/agencies/youngcapital/assets.py
+++ staffing_agency_scraper/scraping/agencies/youngcapital/assets.py
@@ -161,8 +161,6 @@ class YoungCapitalScraper(BaseAgencyScraper):
         self.logger.info(f"📄 Evidence URLs: {len(agency.evidence_urls)}")
         self.logger.info("=" * 80)
 
-        with open('all_text.txt', 'w') as f:
-            f.write(all_text)
         
         return agency
     
